c96c2e99b8b495491845a8edb73b468ef022973c
jelito <jrscallanta@gmail.com>
2024-06-06 11:20:10 -0700

    file name change

--- server/api/routes/taskRoutes.js
+++ server/api/routes/taskRoutes.js
@@ -2,7 +2,7 @@ import { Router, json } from "express";
 const router = Router();
 router.use(json());
 
-import { getTasks, postTask, patchTask } from "../database/controller.js";
+import { getTasks, postTask, patchTask } from "../database/taskController.js";
 
 router.get("/", getTasks);
 router.post("/", postTask);
